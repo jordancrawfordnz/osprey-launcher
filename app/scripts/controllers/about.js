5e6df79d0689d896a2729b5114598679aa98e5e3
Jordan Crawford <jordan.crawford@me.com>
2014-12-21 19:00:53 +1300

    After Yeoman setup

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ospreyLauncherApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ospreyLauncherApp
+ */
+angular.module('ospreyLauncherApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ospreyLauncherApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ospreyLauncherApp
+ */
+angular.module('ospreyLauncherApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
