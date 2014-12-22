2c836d7be7fb5e8eec66543219c1e50bad68aebc
Jordan Crawford <jordan.crawford@me.com>
2014-12-22 20:42:58 +1300

    Display main grid

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,9 @@
 angular.module('ospreyLauncherApp')
   .controller('MainCtrl', function ($scope) {
     $scope.applications = [
-      {'name': 'HTML5 Boilerplate'},
-      {'name': 'AngularJS'},
-      {'name': 'Karma'}
+      {'name': 'Movies'},
+      {'name': 'TV'},
+      {'name': 'Stream'},
+      {'name': 'test'},
     ];
   });
