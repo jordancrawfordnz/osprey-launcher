1453b521c75ca79a3f20b27e804f69829ff8d8d7
Jordan Crawford <jordan.crawford@me.com>
2014-12-23 22:55:52 +1300

    Tidy code, add extra data and add data to backend

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,11 +13,18 @@ angular.module('ospreyLauncherApp')
 
   	$scope.makeCurrent = function(toMakeCurrent)
   	{
-		if($scope.current != null) $scope.current.selected = false;
+  		if(toMakeCurrent === undefined)
+  		{
+  			return;
+  		}
+  		if($scope.current !== null)
+		{
+			$scope.current.selected = false;
+		}
 		toMakeCurrent.selected = true;
 		$scope.current = toMakeCurrent;
 		$scope.$apply(); // update the changes on the interface
-  	}
+  	};
 
   	$scope.moveLeft = function()
 	{
@@ -37,17 +44,46 @@ angular.module('ospreyLauncherApp')
 	};
 	$scope.selectKey = function()
 	{
-		alert('no function!');
+		backend.selectItem($scope.current.name);
 	};
 
 	// == Construct ==
-	
+
   	registerMovementNotifyee($scope); // allow key events to come through.
 
   	// define applications
-  	var plex = {'title': 'Movies','name' : 'plex', 'img' : 'images/plex.png', 'selected' : false};
-  	var mediaportal = {'title': 'TV', 'name' : 'mediaportal', 'img' : 'images/mediaportal.png',  'selected' : false};
-  	var kodi = {'title': 'Stream', 'name' : '', 'img' : 'images/kodi.png',  'selected' : false}
+  	var plex = {'title': 'Movies',
+  				'name' : 'plex',
+  				'img' : 'images/plex.png',
+  				'path' : 'C:\\Program Files (x86)\\Plex Home Theater\\Plex Home Theater.exe',
+  				'suspendable' : true,
+  				'keepOpen' : false,
+  				'selected' : false};
+  	var mediaportal = {'title':'TV',
+  						'name' : 'mediaportal',
+  						'img' : 'images/mediaportal.png',
+  						'path' : 'C:\\Program Files (x86)\\Team MediaPortal\\MediaPortal\\MediaPortal.exe',
+		  				'suspendable' : true,
+		  				'keepOpen' : false,
+  						'selected' : false};
+  	var kodi = {'title': 'Stream',
+  				'name' : 'kodi',
+  				'img' : 'images/kodi.png',
+  				'path' : 'C:\\Program Files (x86)\\XBMC\\XBMC.exe',
+  				'suspendable' : true,
+  				'keepOpen' : false,
+  				'selected' : false};
+  	var desktop = {'title': 'Desktop',
+  					'name' : 'desktop',
+  					'selected' : false};
+  	var exit = {'title': 'Exit',
+  				'name' : 'exit',
+  				'selected' : false};
+  	
+  	// setup the selectables with the backend
+  	backend.addApplication(plex.name, plex.path, plex.suspendable, plex.keepOpen);
+  	backend.addApplication(mediaportal.name, mediaportal.path, mediaportal.suspendable, mediaportal.keepOpen);
+  	backend.addApplication(kodi.name, kodi.path, kodi.suspendable, kodi.keepOpen);
 
   	// define relative applications
   	plex.right = mediaportal;
@@ -63,6 +99,5 @@ angular.module('ospreyLauncherApp')
 
   	// define the default option
   	$scope.makeCurrent(plex);
-
     
   });
