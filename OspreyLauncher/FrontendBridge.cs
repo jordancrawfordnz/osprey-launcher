af5dadf63f45f9a4ffb7a6f61c8af16787b449df
jordancrawfordnz <jordan.crawford@me.com>
2014-12-28 13:26:26 +1300

    Remove un-needed code

--- OspreyLauncher/FrontendBridge.cs
+++ OspreyLauncher/FrontendBridge.cs
@@ -41,46 +41,6 @@ namespace OspreyLauncher
         public void AddExitLaunchable(string name)
         {
             LauncherController.GetInstance().AddSelectable(name, ExitSelectable.GetInstance());
-        }
-
-        /*
-         * 
-         *        void setupLaunchables()
-        {
-            // Kitchen PC uses 32-bit versions of everythings
-            // Currently testing: Hiding only
-
-          // launchables.Add(new LaunchableApplication("MediaPortal", "C:\\Program Files\\Team MediaPortal\\MediaPortal\\MediaPortal.exe", true, false));
-           launchables.Add(new LaunchableApplication("MediaPortal","C:\\Program Files (x86)\\Team MediaPortal\\MediaPortal\\MediaPortal.exe",false, true));
-           launchables.Add(new LaunchableApplication("XBMC", "C:\\Program Files (x86)\\XBMC\\XBMC.exe",false,true));
-          // launchables.Add(new LaunchableApplication("XBMC", "C:\\Program Files\\XBMC\\XBMC.exe", true, false));
-         
-            launchables.Add(DesktopLaunchable.GetInstance()); // the launchable to return to the desktop
-        }
-         * 
-         * */
-
-
-
-        /*
-                public List<Selectable> getSelectableItems()
-                {
-                    List<Selectable> toReturn = new List<Selectable>();
-                    foreach (Selectable currentSelectable in getLaunchables())
-                    {
-                        toReturn.Add(currentSelectable);
-                    }
-                    toReturn.Add(ExitSelectable.GetInstance());
-                    return toReturn;
-                }
-               */
-
-        /*
-        public List<Launchable> getLaunchables()
-        {
-            return selectables;
-        }
-        */
-              
+        }              
     }
 }
