20f787b916070549131705a1452b9c36934f7763
Jordan Crawford <jordan.crawford@me.com>
2015-01-11 18:20:16 +1300

    Move configuration to its own file.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,15 +36,21 @@ angular
   .run(function($rootScope,$location) {
     frontend = $rootScope;
 
-    var useMockBackend = false;
-
-    if(useMockBackend)
+    $rootScope.launchableSelect = function()
     {
-      $rootScope.backend = mockBackend;
+      $rootScope.backend.selectItem($rootScope.currentSelectable.name);
+      setTimeout(function(){ $rootScope.$apply(function() { $location.path('/loading'); }); }, 100);
+      // display loading screen, use a slight delay in-case it loads quickly.
     }
-    else
+
+    $rootScope.selectableSelect = function()
     {
-      $rootScope.backend = backend;
+      $rootScope.backend.selectItem($rootScope.currentSelectable.name);
+    }
+
+    $rootScope.linkSelect = function()
+    {
+      $rootScope.$apply(function() { $location.path('/' + $rootScope.currentSelectable.url); });
     }
 
     $rootScope.setupInBackend = function(toSetup)
@@ -56,7 +62,6 @@ angular
     {
       $rootScope.backend.addRestriction(restrictionAppliesTo.name,restricted.name);
     };
-
 
     $rootScope.makeCurrent = function(toMakeCurrent)
     {
@@ -77,113 +82,16 @@ angular
       $rootScope.$apply(function() { $location.path('/'); });
     };
 
-    // == Setup data ==
+    coreConfigData($rootScope);
 
-
-    $rootScope.launchableSelect = function()
+    if($rootScope.useMockBackend)
     {
-      $rootScope.backend.selectItem($rootScope.currentSelectable.name);
-      setTimeout(function(){ $rootScope.$apply(function() { $location.path('/loading'); }); }, 100);
-      // display loading screen, use a slight delay in-case it loads quickly.
-    }
-
-    $rootScope.selectableSelect = function()
-    {
-      $rootScope.backend.selectItem($rootScope.currentSelectable.name);
-    }
-
-    $rootScope.linkSelect = function()
-    {
-      $rootScope.$apply(function() { $location.path('/' + $rootScope.currentSelectable.url); });
-    }
-
-    // define applications
-    var plex = {'title': 'Movies',
-                'name' : 'Plex',
-                'img' : 'images/plex.png',
-                'suspendable' : true,
-                'keepOpen' : false,
-                'onSelect' : $rootScope.launchableSelect,
-                'restriction' : 'Kodi'};
-    var mediaportal = {'title':'TV',
-                       'name' : 'MediaPortal',
-                       'img' : 'images/mediaportal.png',
-                       'suspendable' : true,
-                       'keepOpen' : false,
-                       'onSelect' : $rootScope.launchableSelect};
-    var kodi = {'title': 'Stream',
-                'name' : 'Kodi',
-                'img' : 'images/kodi.png',
-                'suspendable' : true,
-                'keepOpen' : false,
-                'restriction' : 'Plex',
-                'onSelect' : $rootScope.launchableSelect};
-    var desktop = {'title': 'Desktop',
-                   'name' : 'Desktop',
-                   'onSelect' : $rootScope.selectableSelect};
-    var info = {'title': 'Information',
-                'name' : 'Info',
-                'url'  : 'info',
-                'onSelect' : $rootScope.linkSelect};
-    var exit = {'title': 'Exit',
-                'name' : 'exit',
-                'onSelect' : $rootScope.selectableSelect};
-
-    if($rootScope.backend.isx86())
-    {
-      plex.path = 'C:\\Program Files\\Plex Home Theater\\Plex Home Theater.exe';
-      mediaportal.path = 'C:\\Program Files\\Team MediaPortal\\MediaPortal\\MediaPortal.exe';
-      kodi.path = 'C:\\Program Files\\Kodi\\Kodi.exe';
+      $rootScope.backend = mockBackend;
     }
     else
     {
-      plex.path = 'C:\\Program Files (x86)\\Plex Home Theater\\Plex Home Theater.exe';
-      mediaportal.path = 'C:\\Program Files (x86)\\Team MediaPortal\\MediaPortal\\MediaPortal.exe';
-      kodi.path = 'C:\\Program Files (x86)\\Kodi\\Kodi.exe';
+      $rootScope.backend = backend;
     }
 
-    // setup the selectables with the backend
-    $rootScope.setupInBackend(plex);
-    $rootScope.setupInBackend(mediaportal);
-    $rootScope.setupInBackend(kodi);
-
-    $rootScope.backend.addDesktopLaunchable(desktop.name);
-    $rootScope.backend.addExitLaunchable(exit.name);
-
-    // setup restrictions
-    $rootScope.addRestriction(plex,kodi);
-    $rootScope.addRestriction(kodi,plex);
-
-    $rootScope.backend.setupApplicationAutomaticClosing(4*60);
-
-    // define relative applications
-    plex.left = desktop;
-    plex.right = mediaportal;
-
-    mediaportal.left = plex;
-    mediaportal.right = kodi;
-
-    kodi.left = mediaportal;
-    kodi.right = desktop;
-
-    desktop.left = kodi;
-    desktop.right = plex;
-    desktop.up = exit;
-    desktop.down = info;
-
-    info.left = kodi;
-    info.right = plex;
-    info.up = desktop;
-    info.down = exit;
-
-    exit.left = kodi;
-    exit.right = plex;
-    exit.up = info;
-    exit.down = desktop;
-
-    // define display order
-    $rootScope.applications = [plex,mediaportal,kodi];
-    $rootScope.extras = [desktop,info,exit];
-    
-    $rootScope.makeCurrent(plex);
+    setupConfigData($rootScope);
   });
---
+++ app/scripts/configuration.js
@@ -0,0 +1,92 @@
+'use strict';
+var coreConfigData = function($rootScope)
+{
+	$rootScope.useMockBackend = false;
+}
+
+var setupConfigData = function($rootScope)
+{
+	
+	// == Setup data ==
+
+    // define applications
+    var plex = {'title': 'Movies',
+                'name' : 'Plex',
+                'img' : 'images/plex.png',
+                'suspendable' : true,
+                'keepOpen' : false,
+                'onSelect' : $rootScope.launchableSelect,
+                'restriction' : 'Kodi'};
+    var mediaportal = {'title':'TV',
+                       'name' : 'MediaPortal',
+                       'img' : 'images/mediaportal.png',
+                       'suspendable' : true,
+                       'keepOpen' : false,
+                       'onSelect' : $rootScope.launchableSelect};
+    var kodi = {'title': 'Stream',
+                'name' : 'Kodi',
+                'img' : 'images/kodi.png',
+                'suspendable' : true,
+                'keepOpen' : false,
+                'restriction' : 'Plex',
+                'onSelect' : $rootScope.launchableSelect};
+    var desktop = {'title': 'Desktop',
+                   'name' : 'Desktop',
+                   'onSelect' : $rootScope.selectableSelect};
+    var info = {'title': 'Information',
+                'name' : 'Info',
+                'url'  : 'info',
+                'onSelect' : $rootScope.linkSelect};
+
+    if($rootScope.backend.isx86())
+    {
+      plex.path = 'C:\\Program Files\\Plex Home Theater\\Plex Home Theater.exe';
+      mediaportal.path = 'C:\\Program Files\\Team MediaPortal\\MediaPortal\\MediaPortal.exe';
+      kodi.path = 'C:\\Program Files\\Kodi\\Kodi.exe';
+    }
+    else
+    {
+      plex.path = 'C:\\Program Files (x86)\\Plex Home Theater\\Plex Home Theater.exe';
+      mediaportal.path = 'C:\\Program Files (x86)\\Team MediaPortal\\MediaPortal\\MediaPortal.exe';
+      kodi.path = 'C:\\Program Files (x86)\\Kodi\\Kodi.exe';
+    }
+
+    // setup the selectables with the backend
+    $rootScope.setupInBackend(plex);
+    $rootScope.setupInBackend(mediaportal);
+    $rootScope.setupInBackend(kodi);
+
+    $rootScope.backend.addDesktopLaunchable(desktop.name);
+
+    // setup restrictions
+    $rootScope.addRestriction(plex,kodi);
+    $rootScope.addRestriction(kodi,plex);
+
+    $rootScope.backend.setupApplicationAutomaticClosing(4*60);
+
+    // define relative applications
+    plex.left = desktop;
+    plex.right = mediaportal;
+
+    mediaportal.left = plex;
+    mediaportal.right = kodi;
+
+    kodi.left = mediaportal;
+    kodi.right = desktop;
+
+    desktop.left = kodi;
+    desktop.right = plex;
+    desktop.up = info;
+    desktop.down = info;
+
+    info.left = kodi;
+    info.right = plex;
+    info.up = desktop;
+    info.down = desktop;
+
+    // define display order
+    $rootScope.applications = [plex,mediaportal,kodi];
+    $rootScope.extras = [desktop,info];
+    
+    $rootScope.makeCurrent(plex);
+}
