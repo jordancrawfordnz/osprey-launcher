79b9486e74d6823ae27289164b8bc579c11e3569
Jordan Crawford <jordan.crawford@me.com>
2015-02-06 12:10:19 +1300

    Use real backend.

--- app/scripts/configuration.js
+++ app/scripts/configuration.js
@@ -1,7 +1,7 @@
 'use strict';
 var coreConfigData = function($rootScope)
 {
-	$rootScope.useMockBackend = true;
+	$rootScope.useMockBackend = false;
 };
 
 var setupConfigData = function($rootScope)
